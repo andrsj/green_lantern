010402d2da0a044d370bfebb816964f22529dc1b
andrsj <andrsj.derkach@gmail.com>
2020-03-23 16:21:13 +0200

    Test Failed -> Logic -> Test Pass [Without obstacles]

--- lantern/tdd/lonely_robot.py
+++ lantern/tdd/lonely_robot.py
@@ -1,27 +1,85 @@
 class Asteroid:
-    def __init__(self, x, y):
+    def __init__(self, x:int, y:int):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return f'Asteroid x = {self.x},\ty = {self.y}'
 
 
 class Robot:
-    def __init__(self, x, y, asteroid):
+
+    _directions = ['N', 'W', 'S', 'E']
+
+    def __init__(self, x:int, y:int, asteroid:Asteroid, direction:str):
         self.x = x
         self.y = y
         self.asteroid = asteroid
-        if self.x > self.asteroid.x:
-            raise MissAsteroidError()
+        if direction.upper() in Robot._directions:
+            self.direction = direction.upper()
+        else:
+            raise InvalidDirection
+        if self.x > self.asteroid.x or self.y > self.asteroid.y:
+            raise MissAsteroidError
+
+    def __str__(self):
+        return f'Robot x = {self.x}, y = {self.y}, direction = {self.direction}\nAsteroid: {self.asteroid}'
+
+    def turn_left(self):
+        self.direction = Robot._directions[Robot._directions.index(self.direction) - 1]
+
+    def turn_right(self):
+        self.direction = Robot._directions[(Robot._directions.index(self.direction) + 1) % len(Robot._directions)]
+
+    def __change_coord_value(self, value, x_direction=True):
+        if x_direction:
+            if self.x + value <= self.asteroid.x and self.x >= 0:
+                self.x += value
+            else:
+                raise FallingRobot
+        else:
+            if self.y + value <= self.asteroid.y and self.y >= 0:
+                self.y += value
+            else:
+                raise FallingRobot
+
+    def move_forward(self, value=1):
+        moves = {
+            'N': (self.__change_coord_value, {'value': value, 'x_direction': False}),
+            'W': (self.__change_coord_value, {'value': value}),
+            'S': (self.__change_coord_value, {'value': -value, 'x_direction': False}),
+            'E': (self.__change_coord_value, {'value': -value})
+        }
+        moves[self.direction][0](**moves[self.direction][1])
+
+    def move_backward(self, value=1):
+        moves = {
+            'N': (self.__change_coord_value, {'value': -value, 'x_direction': False}),
+            'W': (self.__change_coord_value, {'value': -value}),
+            'S': (self.__change_coord_value, {'value': value, 'x_direction': False}),
+            'E': (self.__change_coord_value, {'value': value})
+        }
+        moves[self.direction][0](**moves[self.direction][1])
 
 
 class MissAsteroidError(Exception):
     pass
 
+
+class InvalidDirection(Exception):
+    pass
+
+
+class FallingRobot(Exception):
+    pass
+
+
 # Agenda:
 #
-#     Create Robot with position and direction
-#     Check if Robot miss asteroid while landing
-#     Create and test turn_left and turn_right functions
-#     Add move_forward, move_backward functions
-#     Check if it falls from asteroid during movement
+#     + Create Robot with position and direction
+#     + Check if Robot miss asteroid while landing
+#     + Create and test turn_left and turn_right functions
+#     + Add move_forward, move_backward functions
+#     + Check if it falls from asteroid during movement
 #     Add asteroid obstacles
 #     Update robot movement to respect obstacles
--- lantern/tdd/test_lonely_robot_execution.py
+++ lantern/tdd/test_lonely_robot_execution.py
@@ -1,25 +1,98 @@
 import pytest
-from lantern.tdd.lonely_robot import Robot, Asteroid, MissAsteroidError
+import lonely_robot
 
 
 class TestRobotCreation:
     def test_parameters(self):
         x, y = 10, 15
-        asteroid = Asteroid(x, y)
-        robot = Robot(x, y, asteroid)
+        asteroid = lonely_robot.Asteroid(x, y)
+        robot = lonely_robot.Robot(x, y, asteroid, 'N')
         assert robot.x == 10
         assert robot.y == 15
         assert robot.asteroid == asteroid
 
     @pytest.mark.parametrize(
+        'direction',
+        ('', 'M', 'I', 'Xla', 'L', '2')
+    )
+    def test_check_true_direction(self, direction):
+        asteroid = lonely_robot.Asteroid(10, 10)
+        with pytest.raises(lonely_robot.InvalidDirection):
+            lonely_robot.Robot(5, 5, asteroid, direction)
+
+    @pytest.mark.parametrize(
         "asteroid_size,robot_coordinates",
         (
-                ((15, 25), (26, 30)),
-                ((15, 25), (26, 24)),
-                ((15, 25), (15, 27)),
+            ((15, 25), (26, 30)),
+            ((15, 25), (26, 24)),
+            ((15, 25), (15, 27)),
         )
     )
     def test_check_if_robot_on_asteroid(self, asteroid_size, robot_coordinates):
-        with pytest.raises(MissAsteroidError):
-            asteroid = Asteroid(*asteroid_size)
-            Robot(*robot_coordinates, asteroid)
+        with pytest.raises(lonely_robot.MissAsteroidError):
+            asteroid = lonely_robot.Asteroid(*asteroid_size)
+            lonely_robot.Robot(*robot_coordinates, asteroid, 'N')
+
+
+@pytest.fixture()
+def create_asteroid_and_robot():
+    asteroid = lonely_robot.Asteroid(30, 30)
+    robot = lonely_robot.Robot(15, 15, asteroid, 'N')
+    print('> setUp')
+    print(f'> Created: {robot}')
+    yield asteroid, robot
+    print('> tearDown')
+
+
+class TestMovingRobot:
+
+    @pytest.fixture(autouse=True)
+    def _create_asteroid_and_robot(self, create_asteroid_and_robot):
+        self._asteroid, self._robot = create_asteroid_and_robot
+
+    def test_turn_left(self):
+        robot = self._robot
+        for direction in ['E', 'S', 'W', 'N']:
+            robot.turn_left()
+            assert robot.direction == direction
+
+    def test_turn_right(self):
+        robot = self._robot
+        for direction in ['W', 'S', 'E', 'N']:
+            robot.turn_right()
+            assert robot.direction == direction
+
+    def test_move_forward(self):
+        robot = self._robot
+        coordinates = [
+            (14, 15),
+            (14, 14),
+            (15, 14),
+            (15, 15)
+        ]
+        for coord in coordinates:
+            robot.turn_left()
+            robot.move_forward()
+            assert (robot.x, robot.y) == coord
+
+    def test_move_backward(self):
+        robot = self._robot
+        coordinates = [
+            (16, 15),
+            (16, 16),
+            (15, 16),
+            (15, 15)
+        ]
+        for coord in coordinates:
+            robot.turn_left()
+            robot.move_backward()
+            assert (robot.x, robot.y) == coord
+
+    def test_falls_after_moving(self):
+        robot = self._robot
+        with pytest.raises(lonely_robot.FallingRobot):
+            robot.move_forward(16)
+
+        robot.turn_left()
+        with pytest.raises(lonely_robot.FallingRobot):
+            robot.move_backward(16)
