3f314e140b3cd27e8820d552c3effc0859186e42
andrsj <andrsj.derkach@gmail.com>
2020-05-13 20:27:14 +0300

    First Migrate

--- lantern/flask/flask_migrations/practice/grocery_store/app.py
+++ lantern/flask/flask_migrations/practice/grocery_store/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_script import Server, Manager
+from flask_migrate import MigrateCommand
 
 from grocery_store.config import Config
 from grocery_store.routes import users, goods, stores
@@ -24,4 +24,5 @@ def make_db(app):
 def make_manager(app):
     manager = Manager(app)
     manager.add_command('runserver', Server(host=Config.HOST, port=Config.PORT))
+    manager.add_command('db', MigrateCommand)
     return manager
