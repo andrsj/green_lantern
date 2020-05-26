7ded232b8551e38fb72b79b10a5b9ee42c2d189d
andrsj <andrsj.derkach@gmail.com>
2020-05-26 23:36:51 +0300

    HW#8 Orders done

--- lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
+++ lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
@@ -13,4 +13,22 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', user=current_user.name, email=current_user.email)
+    return render_template(
+        'profile.html', 
+        user=current_user.name, 
+        email=current_user.email
+    )
+
+@main.route('/orders')
+@login_required
+def orders():
+    orders_list = []
+    for order in current_user.orders:
+        order_data = (
+            order.store.name,
+            order.created_time,
+            sum([good.good.price for good in order.order_lines]),
+            {good.good.name: good.good.price for good in order.order_lines}
+        )
+        orders_list.append(order_data)
+    return render_template('orders.html', orders=orders_list)
