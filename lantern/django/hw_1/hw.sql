56f901465ce4b257be70a5a5b3207a4a859d66a0
andrsj <andrsj.derkach@gmail.com>
2020-06-13 16:23:21 +0300

    Refactor names and keys

--- lantern/django/hw_1/hw.sql
+++ lantern/django/hw_1/hw.sql
@@ -1,5 +1,5 @@
-CREATE TABLE Restrn(
-	restrn_id int PRIMARY KEY,
+CREATE TABLE Restaurant(
+	id int PRIMARY KEY,
 	name varchar(25),
 	counrtry_id int,
 	personal_id int,
@@ -10,46 +10,46 @@ CREATE TABLE Personal(
     id int PRIMARY KEY,
     name VARCHAR(30),
     posada VARCHAR(30),
-    restrn_id int REFERENCES Restrn(restrn_id)
+    restaurant_id int REFERENCES Restaurant(id)
 );
 
 CREATE TABLE Country(
-    country_id int PRIMARY KEY,
+    id int PRIMARY KEY,
     name varchar(30)
 );
 
 CREATE TABLE Town(
     id int PRIMARY KEY,
     name varchar(40),
-    counrty_id int REFERENCES Country(country_id)
+    counrty_id int REFERENCES Country(id)
 );
 
-CREATE TABLE RestrnCountry(
-    Restrn_id int,
-    Counrty_id int,
-    FOREIGN KEY (Restrn_id) REFERENCES Restrn(restrn_id),
-    FOREIGN KEY (Counrty_id) REFERENCES Counrty(country_id),
-    UNIQUE (restrn_id, country_id)
+CREATE TABLE RestaurantCountry(
+    restaurant_id int,
+    country_id int,
+    FOREIGN KEY (restaurant_id) REFERENCES Restaurant(id),
+    FOREIGN KEY (country_id) REFERENCES Country(id),
+    UNIQUE (restaurant_id, country_id)
 );
 
 CREATE TABLE Menu(
-    menu_id int PRIMARY KEY,
+    id int PRIMARY KEY,
     name VARCHAR(20),
     start date,
     finish date
 );
 
-CREATE TABLE RestrnMenu(
-    Restrn_id int,
-    Menu_id int,
-    FOREIGN KEY (Restrn_id) REFERENCES Restrn(restrn_id),
-    FOREIGN KEY (Menu_id) REFERENCES Menu(menu_id),
-    UNIQUE (restrn_id, menu_id)
+CREATE TABLE RestaurantMenu(
+    restaurant_id int,
+    menu_id int,
+    FOREIGN KEY (restaurant_id) REFERENCES Restaurant(id),
+    FOREIGN KEY (menu_id) REFERENCES Menu(id),
+    UNIQUE (restaurant_id, menu_id)
 );
 
 CREATE TABLE Soop(
     id int PRIMARY KEY,
     name VARCHAR(20),
-    ingrid VARCHAR(100),
-    menu_id int REFERENCES Menu(menu_id)
+    ingredient VARCHAR(100),
+    menu_id int REFERENCES Menu(id)
 );
