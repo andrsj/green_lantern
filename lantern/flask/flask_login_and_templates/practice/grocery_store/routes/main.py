01e2fd066622c83e602af4a6f2acc660535b16a4
andrsj <andrsj.derkach@gmail.com>
2020-06-13 15:58:51 +0300

    Remove comments

--- lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
+++ lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
@@ -38,13 +38,4 @@ def orders():
         for order in current_user.orders
     ]
 
-    # orders_list = []
-    # for order in current_user.orders:
-    #     order_data = (
-    #         order.store.name,
-    #         order.created_time,
-    #         sum([good.good.price for good in order.order_lines]),
-    #         {good.good.name: good.good.price for good in order.order_lines}
-    #     )
-    #     orders_list.append(order_data)
     return render_template('orders.html', orders=orders_list)
